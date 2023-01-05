e9fe022d80455acad9d744ef54cc994dd1e9c3ba
TeodoraMaruna <maruna.ra117.2018@uns.ac.rs>
2023-01-05 20:03:59 +0100

    feat: modelUser

--- frontend/src/app/model/user.ts
+++ frontend/src/app/model/user.ts
@@ -1,4 +1,11 @@
 export class User {
     username: String = "";
-    isPublic: boolean = false; 
+    isPublic: boolean = false;
+    password: String = "";
+    role: String = "";
+    firstName: String = "";
+    lastName: String = "";
+    gender: String = "";
+    email: String = "";
+    phoneNumber: String = "";
 }
